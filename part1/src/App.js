aead3db4d02811d0240bdd6cde7238c6fcfe7a8b
omerk42 <omerkhanak42@gmail.com>
2023-04-20 14:43:58 +0300

    part 1 1.1

---
+++ part1/src/App.js
@@ -0,0 +1,42 @@
+const Header = (prop) => {
+  return (
+    <div className="header">
+      <h1>{prop.course}</h1>
+    </div>
+  );
+};
+
+function Content(prop) {
+  const obj = prop.object;
+  return obj.map((ob) => (
+    <p id={ob.name}>
+      {ob.name} {ob.exe}
+    </p>
+  ));
+}
+
+function Total(prop) {
+  let a = 0;
+  const obj = prop.object;
+  obj.map((ob) => (a = ob.exe + a));
+  return <p>Number of exercises {a}</p>;
+}
+
+const App = () => {
+  const course = "Half Stack application development";
+  const object = [
+    { name: "Fundamentals of React", exe: 10 },
+    { name: "Using props to pass data", exe: 7 },
+    { name: "State of a component", exe: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content object={object} />
+      <Total object={object} />
+    </div>
+  );
+};
+
+export default App;
