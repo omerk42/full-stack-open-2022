86a525f13f36398a88716bd1b2fa3d1bb2106fdd
omerk42 <omerkhanak42@gmail.com>
2023-04-24 08:46:33 +0300

    part 1 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -22,19 +22,50 @@ function Total(prop) {
   return <p>Number of exercises {a}</p>;
 }
 
+// const App = () => {
+//   const course = "Half Stack application development";
+//   const object = [
+//     { name: "Fundamentals of React", exe: 10 },
+//     { name: "Using props to pass data", exe: 7 },
+//     { name: "State of a component", exe: 14 },
+//   ];
+
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content object={object} />
+//       <Total object={object} />
+//     </div>
+//   );
+// };
+
 const App = () => {
   const course = "Half Stack application development";
-  const object = [
-    { name: "Fundamentals of React", exe: 10 },
-    { name: "Using props to pass data", exe: 7 },
-    { name: "State of a component", exe: 14 },
-  ];
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content object={object} />
-      <Total object={object} />
+      <p>{course}</p>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
     </div>
   );
 };
