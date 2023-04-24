e9cf3978c674cbd8742f23a31185f05018d6a047
omerk42 <omerkhanak42@gmail.com>
2023-04-24 08:53:39 +0300

    part 1 1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -7,18 +7,18 @@ const Header = (prop) => {
 };
 
 function Content(prop) {
-  const obj = prop.object;
+  const obj = prop.parts;
   return obj.map((ob) => (
     <part id={ob.name}>
-      {ob.name} {ob.exe}
+      {ob.name} {ob.exercises}
     </part>
   ));
 }
 
 function Total(prop) {
   let a = 0;
-  const obj = prop.object;
-  obj.map((ob) => (a = ob.exe + a));
+  const obj = prop.parts;
+  obj.map((ob) => (a = ob.exercises + a));
   return <p>Number of exercises {a}</p>;
 }
 
@@ -41,31 +41,26 @@ function Total(prop) {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
-      <p>{course}</p>
-      <p>
-        {part1.name} {part1.exercises}
-      </p>
-      <p>
-        {part2.name} {part2.exercises}
-      </p>
-      <p>
-        {part3.name} {part3.exercises}
-      </p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
